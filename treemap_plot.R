a19d327c7540e678ffd978a08fdf1156fb99ce10
Simon Peters <smellons29@gmail.com>
2023-09-08 05:57:51 -0400

    Add Treemap

---
+++ treemap_plot.R
@@ -0,0 +1,37 @@
+### TREEMAP PLOT
+
+# load libraries
+library(tidyverse)
+library(treemap)
+
+# check out the dataset.
+glimpse(mpg)
+
+# change variables used  for plotting into factors
+mpg <- mpg %>% 
+  mutate(class = as_factor(class),
+         manufacturer = as_factor(manufacturer)) %>%
+  relocate(class, .after = model)
+
+# create a table to see where each category matches up to manufacturer
+table(mpg$class, mpg$manufacturer)
+
+# create a tibble with the selected count
+car_count <- mpg %>% 
+  count(class, manufacturer)
+
+# create a treemap with visual options.
+treemap(car_count,
+        index=c("class","manufacturer"),
+        vSize="n",
+        type="index",
+        title="Car Manufacturer by Class Size",
+        fontsize.title = 12,        
+        fontsize.labels=c(15,11),   
+        fontcolor.labels="black",  
+        fontface.labels=c(2,2),  
+        overlap.labels=1,          
+        palette="Dark2",         #RColorBrewer::display.brewer.all()
+        align.labels=         
+          c("center", "center")) 
+
